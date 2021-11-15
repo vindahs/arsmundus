e49513645243ef8332b23018ebd3b63593d0690d
vindahs <vincentonoja@ymail.com>
2021-11-15 12:04:56 +0100

    first commit

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -6,55 +6,51 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
-    res.render('about', { title: 'Express' });
-});
 
-router.get('/blog-single', function(req, res, next) {
-    res.render('blog-single', { title: 'Express' });
-});
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Express' });
 });
-router.get('/gallery', function(req, res, next) {
-    res.render('gallery', { title: 'Express' });
+router.get('/Oceano', function(req, res, next) {
+    res.render('Oceano', { title: 'Express' });
 });
 router.get('/main', function(req, res, next) {
     res.render('main', { title: 'Express' });
 });
-router.get('/gallery', function(req, res, next) {
-    res.render('gallery', { title: 'Express' });
+
+router.get('/OMG', function(req, res, next) {
+    res.render('OMG', { title: 'Express' });
+});
+
+router.get('/purpletana', function(req, res, next) {
+    res.render('purpletana', { title: 'Express' });
+});
+
+router.get('/hamu', function(req, res, next) {
+    res.render('hamu', { title: 'Express' });
+});
+
+router.get('/myessence', function(req, res, next) {
+    res.render('myessence', { title: 'Express' });
 });
 
 
-router.get('/gallery2', function(req, res, next) {
-    res.render('gallery2', { title: 'Express' });
-});
+router.get('/hamu', function(req, res, next) {
+    res.render('hamu', { title: 'Express' });
+})
 
-router.get('/gallery3', function(req, res, next) {
-    res.render('gallery3', { title: 'Express' });
-});
 
-router.get('/gallery4', function(req, res, next) {
-    res.render('gallery4', { title: 'Express' });
-});
-
-router.get('/gallery5', function(req, res, next) {
-    res.render('gallery5', { title: 'Express' });
-});
-
-router.get('/gallery6', function(req, res, next) {
-    res.render('gallery6', { title: 'Express' });
+router.get('/saviesa', function(req, res, next) {
+    res.render('saviesa', { title: 'Express' });
 });
 
 router.get('/ovgallery', function(req, res, next) {
     res.render('ovgallery', { title: 'Express' });
 });
 
-
-router.get('/blog', function(req, res, next) {
-    res.render('blog', { title: 'Express' });
+router.get('/burna', function(req, res, next) {
+    res.render('burna', { title: 'Express' });
 });
+
 
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Express' });
